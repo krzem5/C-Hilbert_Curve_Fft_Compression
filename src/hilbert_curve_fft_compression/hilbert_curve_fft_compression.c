262c70eed1734bee62a53f8c1b29a838dcc8c684
Krzem <krzem5.dev@gmail.com>
2021-10-17 08:19:55 +0200

    Push Update 17/10/2021, 08:19:55

--- src/hilbert_curve_fft_compression/hilbert_curve_fft_compression.c
+++ src/hilbert_curve_fft_compression/hilbert_curve_fft_compression.c
@@ -3,10 +3,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 
 
-#define MIN_AMPLITUDE 5
+#define MIN_AMPLITUDE 0
 
 
 
@@ -43,48 +44,50 @@ static void _sort(epicycle_t* a,uint16_t i,uint16_t j){
 
 
 void compress_matrix(uint8_t* bf,compressed_data_t* o){
-	epicycle_t e[MATRIX_SIZE*MATRIX_SIZE];
+	uint8_t dt[MATRIX_SIZE*MATRIX_SIZE];
+	for (uint16_t i=0;i<MATRIX_SIZE*MATRIX_SIZE;i++){
+		uint16_t x=0;
+		uint16_t y=0;
+		uint16_t t=i;
+		for (uint8_t s=1;s<MATRIX_SIZE;s<<=1){
+			uint8_t ry=t&1;
+			t>>=1;
+			uint8_t rx=t&1;
+			t>>=1;
+			if (ry^rx){
+				y+=s;
+			}
+			else{
+				if (rx){
+					x=s-x-1;
+					y=s-y-1;
+				}
+				uint16_t t=x;
+				x=y;
+				y=t;
+			}
+			x+=s*rx;
+		}
+		dt[i]=*(bf+y*MATRIX_SIZE+x);
+	}
+	epicycle_t e[MATRIX_SIZE*MATRIX_SIZE-1];
 	uint16_t i=0;
 	float a=0;
 	for (uint16_t j=0;j<MATRIX_SIZE*MATRIX_SIZE;j++){
 		float re=0;
 		float im=0;
 		for (uint16_t k=0;k<MATRIX_SIZE*MATRIX_SIZE;k++){
-			uint16_t x=0;
-			uint16_t y=0;
-			uint16_t t=k;
-			for (uint8_t s=1;s<MATRIX_SIZE;s<<=1){
-				uint8_t ry=t&1;
-				t>>=1;
-				uint8_t rx=t&1;
-				t>>=1;
-				if (ry^rx){
-					y+=s;
-				}
-				else{
-					if (rx){
-						x=s-x-1;
-						y=s-y-1;
-					}
-					uint16_t t=x;
-					x=y;
-					y=t;
-				}
-				x+=s*rx;
-			}
-			re+=(*(bf+y*MATRIX_SIZE+x))*cosf(a*k);
-			im+=(*(bf+y*MATRIX_SIZE+x))*sinf(a*k);
+			re+=dt[k]*cosf(a*k);
+			im+=dt[k]*sinf(a*k);
 		}
 		a+=6.283185307179586f/(MATRIX_SIZE*MATRIX_SIZE);
 		if (!j){
 			o->off=sqrtf(re*re+im*im)/(MATRIX_SIZE*MATRIX_SIZE);
 			continue;
 		}
-		re/=MATRIX_SIZE*MATRIX_SIZE;
-		im/=MATRIX_SIZE*MATRIX_SIZE;
 		e[i].r=re*re+im*im;
-		if (e[i].r>=MIN_AMPLITUDE*MIN_AMPLITUDE){
-			e[i].r=sqrtf(e[i].r);
+		if (e[i].r>=MIN_AMPLITUDE*MIN_AMPLITUDE*MATRIX_SIZE*MATRIX_SIZE*MATRIX_SIZE*MATRIX_SIZE){
+			e[i].r=sqrtf(e[i].r)/(MATRIX_SIZE*MATRIX_SIZE);
 			e[i].s=atan2f(-im,re);
 			e[i].f=j;
 			i++;
